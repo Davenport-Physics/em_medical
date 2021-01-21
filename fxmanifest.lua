2b57dc9e643881ebf747e50a7c058f92f36a41b1
Emma Davenport <davenport.physics@gmail.com>
2021-01-21 12:16:10 -0600

    Moved wounds code to wounds directory

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,11 +9,14 @@ server_scripts {
 client_scripts {
     'client/prototype.lua',
     'client/config.lua',
-    'client/wounds.lua',
+    'client/wounds/wounds.lua',
     'client/effects/effects_core.lua',
     'client/effects/effects.lua',
     'client/player/player.lua',
     'client/misc/unconscious_manager.lua',
     'client/libs.lua',
-    'client/blood.lua'
+    'client/blood.lua',
+    'client/reactions/reactions_core.lua',
+    'client/reactions/bone_reactions.lua',
+    'client/reactions/weapon_reactions.lua'
 }
