784fafb767ceaad6694dbf12f94852c072b4193e
Emma Davenport <davenport.physics@gmail.com>
2021-01-21 16:14:15 -0600

    Change: Add the opportunity of going into shock, if you have been tazed. If you are on meth, there is a higher chance of going into shock.

--- client/reactions/weapon_reactions.lua
+++ client/reactions/weapon_reactions.lua
@@ -11,6 +11,16 @@ local function stun_gun_reaction()
 		apply_short_term_effect(EFFECTS.TAZED)
 	end
 
+	if PLAYER.SHORTERM_EFFECTS["Tazed"] ~= nil then
+
+		if math.random() <= 0.05 then
+			apply_short_term_effect(EFFECTS.SHOCK)
+		elseif math.random() <= 0.20 and PLAYER.SHORTERM_EFFECTS["Meth"] ~= nil then
+			apply_short_term_effect(EFFECTS.SHOCK)
+		end
+
+	end
+
 end
 
 register_weapon_reaction(WEAPON_HASHES.WEAPON_STUNGUN, stun_gun_reaction)
